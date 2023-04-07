5a105a5b9b17cccb0381cecd870d95adf58509ae
Nitinkuvalekar <kuvalekar8@gmail.com>
2023-04-07 09:57:09 +0530

    prod vite build

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
     setloading(true);
 
     try {
-      const response = await fetch("http://localhost:8080/api/v1/post", {
+      const response = await fetch("https://dall-e-uhnl.onrender.com/api/v1/post", {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
